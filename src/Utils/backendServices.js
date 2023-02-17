3fffb5548e5494bdb6ff21c2d6a306debd5b133c
Shehreen Fatima <shehreen.fatima@pinely.eu>
2023-02-17 17:03:18 +0500

    data fetched from api

---
+++ src/Utils/backendServices.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const getRecipes = (searchedRecipe) => {
+  let response = axios.get(
+    `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchedRecipe}`
+  );
+
+  return response;
+};
+
+export const getRecipeDetails = (searchedRecipeId) => {
+  let response = axios.get(
+    `https://forkify-api.herokuapp.com/api/v2/recipes/${searchedRecipeId}`
+  );
+  return response;
+};
+
+export const addRecipe = (recipe, newRecipe) => {
+  let apiKey = "077c720f-4aef-4a18-ba8f-e917e208b9b0";
+
+  let response = axios.post(
+    `https://forkify-api.herokuapp.com/api/v2/recipes?search=${recipe}&key=${apiKey}`,
+    newRecipe
+  );
+};
